59b1c6ef1b6f85371f09615102673b9795bb30a2
Daniel Toll <daniel@familjentoll.se>
2013-05-09 15:24:36 +0200

    Add sensor code
    Sensor code for the Sharp sensor and the HC_SR04

---
+++ Sensors/SharpSensor.h
@@ -0,0 +1,49 @@
+#ifndef SHARP_SENSOR
+#define SHARP_SENSOR
+
+#include "Sensor.h"
+
+class SharpSensor : public Sensor {
+  public:
+     SharpSensor(int a_analogPin) {
+        m_analogPin = a_analogPin;
+     }
+     
+     static const int minDistance = 16;
+  
+  
+  float getRawMeasurement() {
+      int sensorValue = analogRead(m_analogPin);
+      return (float)sensorValue * (5.0f / 1023.0f);
+  }
+  
+  
+  float transformToCM(float a_voltage) {
+      static const int numDots = 11;
+      static const float voltages[numDots]= {0.60f, 0.65f,  0.71f, 0.77f, 0.85, 0.95f, 1.09, 1.31, 1.63f, 2.11f, 2.9f};
+      static const float distance[numDots]= {120,    110,    100,    90,    80,   70,  60,   50,      40,    30,   16};
+      float dist = 0.0f;
+     
+      for (int i = 0; i < numDots-1; i++) {
+        if (a_voltage < voltages[i+1]) {
+           return floatMap(a_voltage, voltages[i], voltages[i+1], distance[i], distance[i+1]);
+        }
+      }
+      return dist;
+    }
+  
+  protected:
+    int m_analogPin;
+    
+    float floatMap(float sourceValue, float sourceMin, float sourceMax, float destMin, float destMax) {
+      float sourceRange = sourceMax - sourceMin;
+      float percent = ((sourceValue - sourceMin ) / sourceRange);
+      float destRange = destMax - destMin;
+      return destMin + percent * destRange;
+    }
+    
+    
+
+};
+
+#endif
